003259adf45507055c70359871131dd7ba9b828a
m19e <nexus.me7290@gmail.com>
2023-03-19 18:20:59 +0900

    refactor: extract urls to const

---
+++ consts/url.ts
@@ -0,0 +1,10 @@
+export const BASE_URL = "https://bluearchive.wikiru.jp/";
+
+export const AORECO_URL_FIRST =
+  "https://bluearchive.wikiru.jp/?Twitter%E9%80%A3%E8%BC%89/%E3%81%82%E3%81%8A%E3%81%AF%E3%82%8B%E3%83%AC%E3%82%B3%E3%83%BC%E3%83%89/001%EF%BD%9E010%E8%A9%B1";
+
+export const JA_URL_FIRST =
+  "https://bluearchive.wikiru.jp/?Twitter%E9%80%A3%E8%BC%89/%E3%81%B6%E3%82%8B%E3%83%BC%E3%81%82%E3%83%BC%E3%81%8B%E3%81%84%E3%81%B6%E3%81%A3%EF%BC%81/001%EF%BD%9E010%E8%A9%B1";
+
+export const EN_URL_FIRST =
+  "https://bluearchive.wikiru.jp/?Twitter%E9%80%A3%E8%BC%89/English/Official+4-Panel+Manga%28%E5%85%AC%E5%BC%8F4%E3%82%B3%E3%83%9E%E6%BC%AB%E7%94%BB%29/001%EF%BD%9E010";
--- main.ts
+++ main.ts
@@ -2,14 +2,7 @@ import ky from "ky";
 import { DOMParser } from "dom";
 import type { Element } from "dom";
 
-const _AORECO_URL_FIRST =
-  "https://bluearchive.wikiru.jp/?Twitter%E9%80%A3%E8%BC%89/%E3%81%82%E3%81%8A%E3%81%AF%E3%82%8B%E3%83%AC%E3%82%B3%E3%83%BC%E3%83%89/001%EF%BD%9E010%E8%A9%B1";
-
-const _JA_URL_FIRST =
-  "https://bluearchive.wikiru.jp/?Twitter%E9%80%A3%E8%BC%89/%E3%81%B6%E3%82%8B%E3%83%BC%E3%81%82%E3%83%BC%E3%81%8B%E3%81%84%E3%81%B6%E3%81%A3%EF%BC%81/001%EF%BD%9E010%E8%A9%B1";
-
-const _EN_URL_FIRST =
-  "https://bluearchive.wikiru.jp/?Twitter%E9%80%A3%E8%BC%89/English/Official+4-Panel+Manga%28%E5%85%AC%E5%BC%8F4%E3%82%B3%E3%83%9E%E6%BC%AB%E7%94%BB%29/001%EF%BD%9E010";
+import { BASE_URL, JA_URL_FIRST } from "/consts/url.ts";
 
 const getHtmlUtf8 = async (res: Response): Promise<string> => {
   const resBuf = await res.arrayBuffer();
