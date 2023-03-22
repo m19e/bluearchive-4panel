6be5a5dffa340ee43d76402f9fa6d63aa599000f
m19e <nexus.me7290@gmail.com>
2023-03-22 23:09:36 +0900

    feat: get ja/en students json

--- dev.ts
+++ dev.ts
@@ -1,15 +1,53 @@
 import ky from "ky";
 import { DOMParser } from "dom";
 
-import { AOHARU_RECORD_PANELS, EN_PANELS, JA_PANELS } from "/consts/panel.ts";
 import { EN_STUDENTS } from "/consts/student.ts";
 import { Panel } from "/types/panel.ts";
 import { getHtmlUtf8, sleep, writeJSON } from "/utils/tools.ts";
 import { convertRomanToKana } from "/utils/romanToKana.ts";
 
+const SCHOOLS = {
+  abydos: { id: "abydos", ja: "アビドス", en: "Abydos" },
+  gehenna: { id: "gehenna", ja: "ゲヘナ", en: "Gehenna" },
+  trinity: { id: "trinity", ja: "トリニティ", en: "Trinity" },
+  millennium: { id: "millennium", ja: "ミレニアム", en: "Millennium" },
+  shanhaijing: { id: "shanhaijing", ja: "山海経", en: "Shanhaijing" },
+  red_winter: { id: "red_winter", ja: "レッドウィンター", en: "Red Winter" },
+  srt: { id: "srt", ja: "SRT特殊学園", en: "SRT" },
+  valkyrie: { id: "valkyrie", ja: "ヴァルキューレ", en: "Valkyrie" },
+  arius: { id: "arius", ja: "アリウス", en: "Arius" },
+  hyakkiyako: { id: "hyakkiyako", ja: "百鬼夜行", en: "Hyakkiyako" },
+  etc: { id: "etc", ja: "その他", en: "ETC" },
+} as const;
+
+type SchoolID = keyof typeof SCHOOLS;
+type Student = { id: string; ja: string; en: string; school: SchoolID };
+type StudentData = Record<string, Student>;
+
 const getStudents = (
   panels: Panel[],
 ) => [...new Set(panels.map((p) => p.students).flat())];
+
+const anotherWear: { [key: string]: string } = {
+  "Shun (Kid)": "シュン（幼女）",
+};
+
+const exceptions: { [key: string]: string } = {
+  "Hatsune Miku": "初音ミク",
+  "Kanna": "カンナ",
+  "Mari": "マリー",
+  "Pina": "フィーナ",
+};
+
+const getStudentKanaFromRoman = (roman: string) => {
+  const [name] = roman.split(" (");
+  return anotherWear[roman] || exceptions[name] || convertRomanToKana(name);
+};
+
+const exceptionIds: { [key: string]: string } = {
+  "Shun (Kid)": "shun_kid",
+  "Hatsune Miku": "hatsune_miku",
+};
 
 const getEnCharacters = async () => {
   const res = await ky("https://bluearchive.wiki/wiki/Characters");
@@ -19,11 +57,34 @@ const getEnCharacters = async () => {
     throw new Error("DOM parse failed");
   }
 
-  const tds = dom.querySelectorAll("tbody > tr > td:nth-child(2)");
-  const students = [...tds].map((s) => s.textContent).filter((t) => t);
-  console.log(students);
+  const tbody = dom.querySelector("tbody")!;
+  const count = tbody?.querySelectorAll("tr")!.length;
+  const students = [...Array(count - 1)].map((_, index) => {
+    const n = index + 2;
+    const name = tbody.querySelector(
+      `tr:nth-child(${n}) > td:nth-child(2)`,
+    )!.textContent;
+    const school = tbody.querySelector(
+      `tr:nth-child(${n}) > td:nth-child(4)`,
+    )!.textContent;
+    return { name, school };
+  });
 
-  await writeJSON("out/students.en.json", students);
+  const en_keyed = students.reduce(
+    (prev, { name, school }) => {
+      const en = anotherWear[name] ? name : name.split(" (")[0];
+      const id = exceptionIds[name] ?? en.toLowerCase();
+      const ja = getStudentKanaFromRoman(name);
+
+      prev[id] = { id, ja, en, school: convertSchoolToID(school) as SchoolID };
+
+      return prev;
+    },
+    {} as StudentData,
+  );
+
+  await writeJSON("out/students.en.json", en_keyed);
+  await sleep(5000);
 };
 
 const getJaCharacters = async () => {
@@ -46,26 +107,18 @@ const getJaCharacters = async () => {
   await sleep(5000);
 };
 
-const anotherWear: { [key: string]: string } = {
-  "Shun (Kid)": "シュン（幼女）",
+const convertSchoolToID = (school: string) =>
+  school.split(" ").join("_").toLowerCase();
+
+const convert = () => {
+  const ja = Object.values(EN_STUDENTS as StudentData).reduce(
+    (prev, student) => {
+      prev[student.ja] = student;
+      return prev;
+    },
+    {} as StudentData,
+  );
+  console.log(ja);
 };
 
-const exceptions: { [key: string]: string } = {
-  "Hatsune Miku": "初音ミク",
-  "Kanna": "カンナ",
-  "Mari": "マリー",
-  "Pina": "フィーナ",
-};
-
-const getStudentKanaFromRoman = (roman: string) => {
-  const [name] = roman.split(" (");
-  return anotherWear[roman] || exceptions[name] || convertRomanToKana(name);
-};
-
-console.log(
-  JSON.stringify(
-    [...new Set(EN_STUDENTS.map(getStudentKanaFromRoman))],
-    null,
-    2,
-  ),
-);
+convert();
