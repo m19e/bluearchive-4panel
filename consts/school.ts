15a3db77079e449e11d9674d112200ac3aa63d71
m19e <nexus.me7290@gmail.com>
2023-03-23 21:45:41 +0900

    refactor: extract schools const

---
+++ consts/school.ts
@@ -0,0 +1,13 @@
+export const SCHOOLS = {
+  abydos: { id: "abydos", ja: "アビドス", en: "Abydos" },
+  gehenna: { id: "gehenna", ja: "ゲヘナ", en: "Gehenna" },
+  trinity: { id: "trinity", ja: "トリニティ", en: "Trinity" },
+  millennium: { id: "millennium", ja: "ミレニアム", en: "Millennium" },
+  shanhaijing: { id: "shanhaijing", ja: "山海経", en: "Shanhaijing" },
+  red_winter: { id: "red_winter", ja: "レッドウィンター", en: "Red Winter" },
+  srt: { id: "srt", ja: "SRT特殊学園", en: "SRT" },
+  valkyrie: { id: "valkyrie", ja: "ヴァルキューレ", en: "Valkyrie" },
+  arius: { id: "arius", ja: "アリウス", en: "Arius" },
+  hyakkiyako: { id: "hyakkiyako", ja: "百鬼夜行", en: "Hyakkiyako" },
+  etc: { id: "etc", ja: "その他", en: "ETC" },
+} as const;
