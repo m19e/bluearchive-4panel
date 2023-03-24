744f88fed02af2c4c8052cb6b00a443ae0ccab16
m19e <nexus.me7290@gmail.com>
2023-03-24 16:18:11 +0900

    refactor: extract types about students

--- types/panel.ts
+++ types/panel.ts
@@ -1,6 +1,12 @@
+import { SCHOOLS } from "/consts/school.ts";
+
 export type Panel = {
   id: string;
   title: string;
   students: string[];
   href: string;
 };
+
+export type SchoolID = keyof typeof SCHOOLS;
+export type Student = { id: string; ja: string; en: string; school: SchoolID };
+export type StudentData = Record<string, Student>;
