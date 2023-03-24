ad70c4caf2f3db012e61ddcb92f33868cf2850b0
m19e <nexus.me7290@gmail.com>
2023-03-24 16:18:52 +0900

    feat: typed students json

--- consts/student.ts
+++ consts/student.ts
@@ -1,4 +1,7 @@
-import EN_STUDENTS from "/out/students.en.json" assert { type: "json" };
-import JA_STUDENTS from "/out/students.ja.json" assert { type: "json" };
+import enJson from "/out/students.en.json" assert { type: "json" };
+import jaJson from "/out/students.ja.json" assert { type: "json" };
 
-export { EN_STUDENTS, JA_STUDENTS };
+import { StudentData } from "/types/panel.ts";
+
+export const EN_STUDENTS = enJson as StudentData;
+export const JA_STUDENTS = jaJson as StudentData;
