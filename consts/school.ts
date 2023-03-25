59199e87bfb0163d511ffb9aef92c3671537554a
m19e <nexus.me7290@gmail.com>
2023-03-25 21:52:25 +0900

    feat: add schools for not playable

--- consts/school.ts
+++ consts/school.ts
@@ -1,13 +1,25 @@
 export const SCHOOLS = {
+  kivotos: { id: "kivotos", ja: "キヴォトス", en: "Kivotos" },
+  prime_student_council: {
+    id: "prime_student_council",
+    ja: "連邦生徒会",
+    en: "Prime Student Council",
+  },
   abydos: { id: "abydos", ja: "アビドス", en: "Abydos" },
   gehenna: { id: "gehenna", ja: "ゲヘナ", en: "Gehenna" },
-  trinity: { id: "trinity", ja: "トリニティ", en: "Trinity" },
   millennium: { id: "millennium", ja: "ミレニアム", en: "Millennium" },
+  trinity: { id: "trinity", ja: "トリニティ", en: "Trinity" },
+  hyakkiyako: { id: "hyakkiyako", ja: "百鬼夜行", en: "Hyakkiyako" },
   shanhaijing: { id: "shanhaijing", ja: "山海経", en: "Shanhaijing" },
   red_winter: { id: "red_winter", ja: "レッドウィンター", en: "Red Winter" },
-  srt: { id: "srt", ja: "SRT特殊学園", en: "SRT" },
   valkyrie: { id: "valkyrie", ja: "ヴァルキューレ", en: "Valkyrie" },
   arius: { id: "arius", ja: "アリウス", en: "Arius" },
-  hyakkiyako: { id: "hyakkiyako", ja: "百鬼夜行", en: "Hyakkiyako" },
+  srt: { id: "srt", ja: "SRT", en: "SRT" },
+  kronos: { id: "kronos", ja: "クロノス", en: "Kronos" },
+  others_students: {
+    id: "others_students",
+    ja: "不明",
+    en: "Others Students",
+  },
   etc: { id: "etc", ja: "その他", en: "ETC" },
 } as const;
