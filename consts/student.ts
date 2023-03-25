3c85053e9885de011234b5c9447f1800b1cd5db5
m19e <nexus.me7290@gmail.com>
2023-03-25 21:53:31 +0900

    feat: add npc const from json

--- consts/student.ts
+++ consts/student.ts
@@ -1,7 +1,9 @@
 import enJson from "/out/students.en.json" assert { type: "json" };
 import jaJson from "/out/students.ja.json" assert { type: "json" };
+import jaNpc from "/out/npc/ja.json" assert { type: "json" };
 
-import { StudentData } from "/types/panel.ts";
+import { FandomStudentData, StudentData } from "/types/panel.ts";
 
 export const EN_STUDENTS = enJson as StudentData;
 export const JA_STUDENTS = jaJson as StudentData;
+export const JA_NPCS = jaNpc as FandomStudentData;
