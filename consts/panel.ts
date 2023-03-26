d6fc46d26a29bc793bc715f58198ecb0bf5c15cd
m19e <nexus.me7290@gmail.com>
2023-03-26 20:14:36 +0900

    refactor: remove break line

--- consts/panel.ts
+++ consts/panel.ts
@@ -5,7 +5,5 @@ import aoharuPanels from "/out/panels/aoharu.json" assert { type: "json" };
 import { Panel } from "/types/panel.ts";
 
 export const JA_PANELS = jaPanels as Panel[];
-
 export const EN_PANELS = enPanels as Panel[];
-
 export const AOHARU_RECORD_PANELS = aoharuPanels as Panel[];
