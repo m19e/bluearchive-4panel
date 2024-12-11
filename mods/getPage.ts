627e7317ada7610856548a01beb4e64d3b78cd5c
m19e <m19e.dev@gmail.com>
2024-12-11 20:40:03 +0900

    Fmt tab-size 2

--- mods/getPage.ts
+++ mods/getPage.ts
@@ -7,142 +7,140 @@ import { Panel } from "/types/panel.ts";
 import { getHtmlUtf8, sleep, writeJSON } from "/utils/tools.ts";
 
 const deletedURL =
-    "https://bluearchive.wikiru.jp/?Twitter%E9%80%A3%E8%BC%89/%E3%81%B6%E3%82%8B%E3%83%BC%E3%81%82%E3%83%BC%E3%81%8B%E3%81%84%E3%81%B6%E3%81%A3%EF%BC%81/041%EF%BD%9E050%E8%A9%B1";
+  "https://bluearchive.wikiru.jp/?Twitter%E9%80%A3%E8%BC%89/%E3%81%B6%E3%82%8B%E3%83%BC%E3%81%82%E3%83%BC%E3%81%8B%E3%81%84%E3%81%B6%E3%81%A3%EF%BC%81/041%EF%BD%9E050%E8%A9%B1";
 
 const deletedPanel: Panel = {
-    id: "41",
-    title: "折衷案",
-    students: ["エイミ", "ヒマリ"],
-    href: "https://bluearchive.jp/comics/41/1",
-    deleted: true,
+  id: "41",
+  title: "折衷案",
+  students: ["エイミ", "ヒマリ"],
+  href: "https://bluearchive.jp/comics/41/1",
+  deleted: true,
 };
 
 const mark = "†";
 
 const parseJaTitle = (original: string) => {
-    if (original.includes("予告")) {
-        return { id: "0", title: "予告" };
-    }
-    const text = original.split(mark).join("").trim();
-    const [ep, ...other] = text.split(/\s+/);
-    const id = ep.replace(/[^0-9]/g, "");
-    const title = other.join(" ") || "無題";
-    return { id, title };
+  if (original.includes("予告")) {
+    return { id: "0", title: "予告" };
+  }
+  const text = original.split(mark).join("").trim();
+  const [ep, ...other] = text.split(/\s+/);
+  const id = ep.replace(/[^0-9]/g, "");
+  const title = other.join(" ") || "無題";
+  return { id, title };
 };
 const en1stPanel = {
-    "id": "10001",
-    "title": "1st Anniversary Special Episode",
-    "students": [
-        "ホシノ",
-        "ノノミ",
-        "シロコ",
-        "セリカ",
-        "アヤネ",
-        "モモイ",
-        "ミドリ",
-        "アリス",
-        "ユズ",
-        "ヒフミ",
-        "ハナコ",
-        "アズサ",
-        "コハル",
-        "アロナ",
-    ],
-    "href": "https://twitter.com/en_bluearchive/status/1590179580379566084",
+  "id": "10001",
+  "title": "1st Anniversary Special Episode",
+  "students": [
+    "ホシノ",
+    "ノノミ",
+    "シロコ",
+    "セリカ",
+    "アヤネ",
+    "モモイ",
+    "ミドリ",
+    "アリス",
+    "ユズ",
+    "ヒフミ",
+    "ハナコ",
+    "アズサ",
+    "コハル",
+    "アロナ",
+  ],
+  "href": "https://twitter.com/en_bluearchive/status/1590179580379566084",
 };
 
 const parseEnTitle = (original: string) => {
-    if (original.includes("1st Anniversary Special Episode")) {
-        return en1stPanel;
-    }
-    const text = original.split(mark).join("").trim();
-    const [ep, title] = text.split(/:\s+/);
-    const id = ep.replace(/[^0-9]/g, "");
-    return { id, title: title || "No Title" };
+  if (original.includes("1st Anniversary Special Episode")) {
+    return en1stPanel;
+  }
+  const text = original.split(mark).join("").trim();
+  const [ep, title] = text.split(/:\s+/);
+  const id = ep.replace(/[^0-9]/g, "");
+  return { id, title: title || "No Title" };
 };
 
 const getPanelContents = (
-    { body, index }: { body: Element; index: number },
+  { body, index }: { body: Element; index: number },
 ) => {
-    const container = body.querySelectorAll(
-        `#body .rgn-container`,
-    )[index] as Element;
+  const container = body.querySelectorAll(
+    `#body .rgn-container`,
+  )[index] as Element;
 
-    const students = [
-        ...container.querySelectorAll(".rgn-description a"),
-    ].map((node) => node.textContent);
-    const href = container.querySelector(".rgn-content > blockquote > a")
-        ?.getAttribute("href") ?? undefined;
+  const students = [
+    ...container.querySelectorAll(".rgn-description a"),
+  ].map((node) => node.textContent);
+  const href = container.querySelector(".rgn-content > blockquote > a")
+    ?.getAttribute("href") ?? undefined;
 
-    return { students, href };
+  return { students, href };
 };
 
 const getPanelCanDeleted = (body: Element, index: number): Partial<Panel> => {
-    if (index === 0) {
-        return deletedPanel;
-    }
+  if (index === 0) {
+    return deletedPanel;
+  }
 
-    const h2 = body.querySelector(`h2#content_1_${index}`)?.textContent!;
-    const { id, title } = parseJaTitle(h2);
+  const h2 = body.querySelector(`h2#content_1_${index}`)?.textContent!;
+  const { id, title } = parseJaTitle(h2);
 
-    const containerIndex = index - 1;
-    const { students, href } = getPanelContents({
-        body,
-        index: containerIndex,
-    });
+  const containerIndex = index - 1;
+  const { students, href } = getPanelContents({
+    body,
+    index: containerIndex,
+  });
 
-    return { id, title, students, href };
+  return { id, title, students, href };
 };
 
 const getPanel = (
-    body: Element,
-    index: number,
-    options: { isEnglish: boolean },
+  body: Element,
+  index: number,
+  options: { isEnglish: boolean },
 ): Partial<Panel> => {
-    const h2 = body.querySelector(`h2#content_1_${index}`)?.textContent!;
-    const { id, title } = options.isEnglish
-        ? parseEnTitle(h2)
-        : parseJaTitle(h2);
+  const h2 = body.querySelector(`h2#content_1_${index}`)?.textContent!;
+  const { id, title } = options.isEnglish ? parseEnTitle(h2) : parseJaTitle(h2);
 
-    const { students, href } = getPanelContents({
-        body,
-        index,
-    });
+  const { students, href } = getPanelContents({
+    body,
+    index,
+  });
 
-    return { id, title, students, href };
+  return { id, title, students, href };
 };
 
 export const getPage = async (
-    url: string,
+  url: string,
 ) => {
-    const res = await fetch(url);
-    const html = await getHtmlUtf8(res);
-    const dom = new DOMParser().parseFromString(html, "text/html");
-    if (!dom) {
-        throw new Error("DOM parse failed");
-    }
-    const body = dom.querySelector("#body");
-    if (!body) {
-        throw new Error("#body is not found");
-    }
+  const res = await fetch(url);
+  const html = await getHtmlUtf8(res);
+  const dom = new DOMParser().parseFromString(html, "text/html");
+  if (!dom) {
+    throw new Error("DOM parse failed");
+  }
+  const body = dom.querySelector("#body");
+  if (!body) {
+    throw new Error("#body is not found");
+  }
 
-    const isDeleted = url === deletedURL;
-    const isEnglish = url.includes("English");
+  const isDeleted = url === deletedURL;
+  const isEnglish = url.includes("English");
 
-    const count = body.querySelectorAll("h2").length;
-    const panels = [...Array(count)].map((_, index) => {
-        return isDeleted
-            ? getPanelCanDeleted(body, index)
-            : getPanel(body, index, { isEnglish });
-    });
-    const nextAnchor = body.querySelector("li.navi_right > a");
-    if (nextAnchor === null) {
-        return { panels, nextUrl: undefined };
-    }
-    const nextHref = nextAnchor.getAttribute("href") ?? "";
-    const nextUrl = new URL(nextHref, BASE_URL).href;
+  const count = body.querySelectorAll("h2").length;
+  const panels = [...Array(count)].map((_, index) => {
+    return isDeleted
+      ? getPanelCanDeleted(body, index)
+      : getPanel(body, index, { isEnglish });
+  });
+  const nextAnchor = body.querySelector("li.navi_right > a");
+  if (nextAnchor === null) {
+    return { panels, nextUrl: undefined };
+  }
+  const nextHref = nextAnchor.getAttribute("href") ?? "";
+  const nextUrl = new URL(nextHref, BASE_URL).href;
 
-    await sleep(1000);
+  await sleep(1000);
 
-    return { panels, nextUrl };
+  return { panels, nextUrl };
 };
