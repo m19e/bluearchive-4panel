2616f255cfb2db63e0e0a2b4c25b2f63a0bdaa85
m19e <m19e.dev@gmail.com>
2025-10-24 01:40:48 +0900

    📝 Export en_npc via consts

--- consts/student.ts
+++ consts/student.ts
@@ -1,9 +1,11 @@
 import enPlay from "/docs/students/playable/en.json" with { type: "json" };
 import jaPlay from "/docs/students/playable/ja.json" with { type: "json" };
 import jaNpc from "/docs/students/npc/ja.json" with { type: "json" };
+import enNpc from "/docs/students/npc/en.json" with { type: "json" };
 
 import { FandomStudentData, StudentData } from "/types/panel.ts";
 
 export const EN_PLAYABLE = enPlay as StudentData;
 export const JA_PLAYABLE = jaPlay as StudentData;
 export const JA_NPC = jaNpc as FandomStudentData;
+export const EN_NPC = enNpc as FandomStudentData;
